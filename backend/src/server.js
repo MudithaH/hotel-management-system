545115fb2c2d22c4683be8bda71555271348c569
Muditha Herath <mudithaherath11@gmail.com>
2025-10-22 13:48:42 +0530

    Change listen part in server.js, listen to all port in the container

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,7 +12,7 @@ const startServer = async () => {
       process.exit(1);
     }
 
-    const server = app.listen(PORT, () => {
+    const server = app.listen(PORT, '0.0.0.0', () => {
       console.log(`Server running on port ${PORT}`);
     });
 
